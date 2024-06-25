d3f0c3f830b10bf4647e06e8ff3bd202ac67fdda
Harshitchopde <chopdeharshit@gmail.com>
2024-06-25 22:09:29 +0530

    Porduction working

---
+++ online-player/src/constants/constant.js
@@ -0,0 +1,1 @@
+export const BASE_URL ="https://youtube-clone-backend-853d.onrender.com/api"
--- online-player/src/pages/Home.js
+++ online-player/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import Cart from '../components/Cart'
 import axios from 'axios';
+import { BASE_URL } from '../constants/constant';
 const Container = styled.div`
 
 display: flex;
@@ -10,14 +11,13 @@ flex-wrap: wrap;
 
 `
 function Home({ type }) {
-
     const [videos, setVideos] = useState([])// insilise with empty array
     useEffect(() => {
         const fetchVideos = async () => {
             try {
                 // console.log(type);
 
-                const res = await axios.get(`/video/${type}`);
+                const res = await axios.get(BASE_URL+`/video/${type}`);
                 setVideos(res.data);
                 console.log("Video : ",res.data);
                 
@@ -32,14 +32,14 @@ function Home({ type }) {
     return (
         <Container>
 
-            {/* { videos && videos?.map((video) => (
+            { videos && videos?.map((video) => (
                 <Cart key={video._id} video={video} />
 
             )
 
 
 
-            )} */}
+            )}
 
 
         </Container>
